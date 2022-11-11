d8f123000802b100212d9280830b5df805ba325d
hara <50170789+dirtyman69@users.noreply.github.com>
2022-11-11 17:34:49 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,107 @@
+const board = [];
+const divs = [];
+for (let y = 0; y < 4; y++) {
+  board[y] = [];
+  divs[y] = [];
+  for (let x = 0; x < 4; x++) {
+    board[y][x] = y * 4 + x + 1;
+    divs[y][x] = null;
+  }
+}
+
+board[3][3] = 0;
+
+const init = () => {
+  // 数値をランダムに
+  for (let i = 0; i < 10000; i++) {
+    let from;
+    let to;
+    do {
+      from = Math.trunc(Math.random() * 15);
+      to = Math.trunc(Math.random() * 15);
+    } while (from === to);
+    [
+      board[Math.trunc(from / 4)][from % 4],
+      board[Math.trunc(to / 4)][to % 4]
+    ] = [
+      board[Math.trunc(to / 4)][to % 4],
+      board[Math.trunc(from / 4)][from % 4]
+    ];
+  }
+
+  for (let y = 0; y < 4; y++) {
+    for (let x = 0; x < 4; x++) {
+      const div = document.createElement("div");
+      document.body.appendChild(div);
+      Object.assign(div.style, {
+        position: "absolute",
+        width: "80px",
+        height: "80px",
+        top: `${y * 80}px`,
+        left: `${x * 80}px`,
+        border: "4px solid #f80",
+        backgroundColor: "#840",
+        fontSize: "50px",
+        color: "#fff",
+        display: "flex",
+        aliginItems: "center",
+        justifyContent: "center"
+      });
+      divs[y][x] = div;
+
+      // クリックしたら移動する
+      div.onpointerdown = () => {
+        ondown(x, y);
+      };
+    }
+  }
+};
+
+const showBoard = () => {
+  // クリアを作る
+  let clear = true;
+
+  for (let y = 0; y < 4; y++) {
+    for (let x = 0; x < 4; x++) {
+      // 数値を表示
+      divs[y][x].textContent = board[y][x] || "";
+
+      if (x !== 3 || y !== 3) {
+        if (board[y][x] !== y * 4 + x + 1) {
+          clear = false;
+        }
+      }
+    }
+  }
+
+  if (clear) {
+    ex = 999;
+    for (let y = 0; y < 4; y++) {
+      for (let x = 0; x < 4; x++) {
+        divs[y][x].style.backgroundColor = "#080";
+      }
+    }
+  }
+};
+
+let ex = 3,
+  ey = 3;
+
+const ondown = (x, y) => {
+  //[exとxの差の絶対値] + [eyとyの差の絶対値] === 1
+  //[ex, ey]と[x, y]が隣り合っている時にしか発生しない
+
+  if (Math.abs(ex - x) + Math.abs(ey - y) === 1) {
+    // 入れ替える
+    board[ey][ex] = board[y][x];
+    board[y][x] = 0;
+    ex = x;
+    ey = y;
+    showBoard();
+  }
+};
+
+window.onload = () => {
+  init();
+  showBoard();
+};
